f955dbf1f45e8d23c17d30a80f96f95dae5a12f3
Cristina <cristinayunesm@gmail.com>
2022-12-04 17:52:21 -0500

    fixing about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,7 +15,8 @@ export default function About () {
           <img src={Me} alt='Yuyitax profile' className='me-photo'/>
         </div>
         <p className='about-text'>My name is Cristina Yunes, an American raised in Venezuela, that pursued a Visual Communicator career and became passionate about web design in the process.<br />
-        I help companies and individuals find solutions through digital outlets. I focus on Digital First and 508 Compliant web design (the world needs to be more inclusive, why not start with your website?)</p>
+        I help companies and individuals find solutions through digital outlets. I focus on Digital First and 508 Compliant web design (the world needs to be more inclusive, why not start with your website?)<br />
+        I am currently enrolled in a Web Developer Bootcamp through Georgia Tech University and hoping to graduate by December 19th, 2022.</p>
 
             </section>
             <CTA />
