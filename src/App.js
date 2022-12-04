7b48d99b0f9c38e19cfa5e7989a5734df083152f
Cristina <cristinayunesm@gmail.com>
2022-12-03 23:36:45 -0500

    added global css, sections on html and work header

--- src/App.js
+++ src/App.js
@@ -5,27 +5,42 @@ import Portfolio from "./components/Portfolio";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 
+
 function App() {
-  let Component
-  switch (window.location.pathname) {
-    case "/react-portfolio":
-    Component = <Home />
-    break
-    case "/about":
-      Component = <About />
-      break
-    case "/portfolio":
-      Component = <Portfolio />
-    break
-    default:
-      Component = <h1>404</h1>
-  }
   return (
-    <>
-    <Navbar />
-    {Component}
-    </>
-  ) 
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+        </Routes>
+      </Router>
+    </div>
+  )
 }
+
+// function App() {
+//   let Component
+//   switch (window.location.pathname) {
+//     case "/react-portfolio":
+//     Component = <Home />
+//     break
+//     case "/about":
+//       Component = <About />
+//       break
+//     case "/portfolio":
+//       Component = <Portfolio />
+//     break
+//     default:
+//       Component = <h1>404</h1>
+//   }
+//   return (
+//     <>
+//     <Navbar />
+//     {Component}
+//     </>
+//   ) 
+// }
 
 export default App;
