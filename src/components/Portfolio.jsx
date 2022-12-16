38d0ce6f8c26789e5866cc73ba5d90b927a74495
Cristina <cristinayunesm@gmail.com>
2022-12-15 19:48:33 -0500

    added link

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -8,11 +8,12 @@ const Portfolio = () => {
     <div className='flex flex-col flex-col-3 w-full sm:flex-cols-1 md:flex-cols-2 lg:flex-cols-3 gap-8 items-center content-center mt-8' id='portfolio'>
     <h2 className='text-right text-4xl mt-4'>Portfolio</h2>
         <div className="relative overflow-hidden">
-          <img
+          <a href="https://yuyitax.github.io/TurnipFest/">
+            <img
             className="relative inset-0 h-auto w-[400px] object-cover transition  hover:-translate-y-1 hover:scale-110"
             src={photo}
             alt=""
-          />
+          /></a>
         </div>
 
         <div className="px-6 py-4">
